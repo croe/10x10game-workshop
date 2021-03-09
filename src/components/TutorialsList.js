73405acd1331123218e5edeb7e4ac946d4e7573c
Akihiro Kato <4kihiro.kato@gmail.com>
2021-03-09 12:03:26 +0900

    Setting Tutorial

---
+++ src/components/TutorialsList.js
@@ -0,0 +1,83 @@
+import React, { useState } from "react";
+import { useList } from "react-firebase-hooks/database";
+import TutorialDataService from "../services/TutorialService";
+import Tutorial from "./Tutorial";
+
+const TutorialsList = () => {
+  const [currentTutorial, setCurrentTutorial] = useState(null);
+  const [currentIndex, setCurrentIndex] = useState(-1);
+
+  /* use react-firebase-hooks */
+  const [tutorials, loading, error] = useList(TutorialDataService.getAll());
+
+  const refreshList = () => {
+    setCurrentTutorial(null);
+    setCurrentIndex(-1);
+  };
+
+  const setActiveTutorial = (tutorial, index) => {
+    const { title, description, published } = tutorial.val();
+
+    setCurrentTutorial({
+      key: tutorial.key,
+      title,
+      description,
+      published,
+    });
+
+    setCurrentIndex(index);
+  };
+
+  const removeAllTutorials = () => {
+    TutorialDataService.removeAll()
+      .then(() => {
+        refreshList();
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  };
+
+  return (
+    <div className="list row">
+      <div className="col-md-6">
+        <h4>Tutorials List</h4>
+
+        {error && <strong>Error: {error}</strong>}
+        {loading && <span>Loading...</span>}
+        <ul className="list-group">
+          {!loading &&
+          tutorials &&
+          tutorials.map((tutorial, index) => (
+            <li
+              className={"list-group-item " + (index === currentIndex ? "active" : "")}
+              onClick={() => setActiveTutorial(tutorial, index)}
+              key={index}
+            >
+              {tutorial.val().title}
+            </li>
+          ))}
+        </ul>
+
+        <button
+          className="m-3 btn btn-sm btn-danger"
+          onClick={removeAllTutorials}
+        >
+          Remove All
+        </button>
+      </div>
+      <div className="col-md-6">
+        {currentTutorial ? (
+          <Tutorial tutorial={currentTutorial} refreshList={refreshList} />
+        ) : (
+          <div>
+            <br />
+            <p>Please click on a Tutorial...</p>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default TutorialsList;
